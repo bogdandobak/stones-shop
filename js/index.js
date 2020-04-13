616599e212490ab3a15f01fdbcd2dd94e2463e30
bogdandobak <weber_bogdan@i.ua>
2020-04-13 19:26:50 +0300

    Make some css code and add simple js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
-function helloWorld() {
-  var str = 'Hello World';
-  console.log(str);
+document.querySelector('.buy-shop').addEventListener('click', getAjaxHtml);
+
+function getAjaxHtml() {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      document.querySelector('.container').innerHTML = xhr.responseText;
+    }
+  };
+  xhr.open('get', 'form.html', true);
+  xhr.send();
 }
-helloWorld()
