0bf723be6ec5d5ee818879e8535eaba776f2fcee
bogdandobak <weber_bogdan@i.ua>
2020-04-16 19:33:16 +0300

    Sample code

--- js/index.js
+++ js/index.js
@@ -4,24 +4,25 @@ const buttonClassic = document.querySelector('.button-classic');
 const buttonAnti = document.querySelector('.button-anti');
 const buttonWhiskey = document.querySelector('.button-whiskey');
 
-const span = document.getElementsByClassName('close-button')[0];
+const closeButton = document.getElementsByClassName('close-button')[0];
 
-buttonClassic.onclick = function () {
+buttonClassic.addEventListener('click', function () {
   modal.style.display = 'block';
-};
-buttonAnti.onclick = function () {
-  modal.style.display = 'block';
-};
-buttonWhiskey.onclick = function () {
-  modal.style.display = 'block';
-};
+});
 
-span.onclick = function () {
+buttonAnti.addEventListener('click', function () {
+  modal.style.display = 'block';
+});
+buttonWhiskey.addEventListener('click', function () {
+  modal.style.display = 'block';
+});
+
+closeButton.addEventListener('click', function () {
   modal.style.display = 'none';
-};
+});
 
-window.onclick = function (event) {
-  if (event.target == modal) {
+window.addEventListener('click', function (event) {
+  if (event.target === modal) {
     modal.style.display = 'none';
   }
-};
+});
